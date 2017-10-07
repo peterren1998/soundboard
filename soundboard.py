af8d902b2719a752999df4bb52fcf7c836151c62
Peter <peterren@berkeley.edu>
2017-10-07 14:53:33 -0700

    added initial soundboard.py code

---
+++ soundboard.py
@@ -0,0 +1,17 @@
+@app.route("/")
+
+class Package:
+
+  def __init__(self, name):
+    self.name = name
+    self.buttons = {}
+
+  def setName(self, name):
+    self.name = name
+
+  def addButton(self, button):
+    self.buttons.add(button)
+
+  def add
+
+class Button:
